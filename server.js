cd3c757a98f6bc0b0507af7376f52b63f057b85f
Jay Ullman <jayullman@gmail.com>
2017-04-09 14:17:00 -0700

    Set up express

--- server.js
+++ server.js
@@ -12,3 +12,15 @@
 https://www.googleapis.com/customsearch/v1?key=AIzaSyBf8ZMDOePSdOUWLQ0bxi0ll3Vs37UcDNM&cx=012894019053467490541:qb36mkgv7wu&searchType=image&q=cats
 */
 
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hey dude!');
+});
+
+app.listen(3000, () => {
+  console.log('App is listening on port: ', port);
+});
