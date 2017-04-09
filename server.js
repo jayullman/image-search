bbdea9ac638a26333439a3648aa6950f35842a17
Jay Ullman <jayullman@gmail.com>
2017-04-09 14:49:56 -0700

    Setup mongoose

--- server.js
+++ server.js
@@ -15,7 +15,45 @@ https://www.googleapis.com/customsearch/v1?key=AIzaSyBf8ZMDOePSdOUWLQ0bxi0ll3Vs3
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const mongoUrl = process.env.MONGOLAB_URI;
+const options = process.env.MONGOLAB_URI
+  ? {
+    server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+  }
+  : {};
+
+mongoose.connect(mongoUrl, options);
+
+mongoose.connection.once('open', console.log.bind(console, 'Successfully connected to database'))
+mongoose.connection.on('error', console.error.bind(console, 'connection error: '));
+
+
 const port = process.env.PORT || 3000
+
+const recentSearchSchema = mongoose.Schema({
+  term: String,
+  when: String
+});
+
+const RecentSearch = mongoose.model('recentSearch', recentSearchSchema);
+
+// create new recent search
+// !!! Test Data !!!
+const recentSearch = new RecentSearch({
+  term: 'lulz',
+  when: new Date().toUTCString()
+});
+
+recentSearch.save((err, record) => {
+  console.log('Record saved: ', record);
+})
+
+console.log(recentSearch);
 
 app.get('/', (req, res) => {
   res.send('Hey dude!');
