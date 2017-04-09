120474c6c98ffc5b7eddc6b0dcedac6a83835931
Jay Ullman <jayullman@gmail.com>
2017-04-09 11:37:07 -0700

    Add custom search api information

---
+++ server.js
@@ -0,0 +1,14 @@
+// API Key: AIzaSyBf8ZMDOePSdOUWLQ0bxi0ll3Vs37UcDNM
+// Search Engine ID: 012894019053467490541:qb36mkgv7wu
+
+// Search URI: https://www.googleapis.com/customsearch/v1?parameters
+
+// Custom Search URI: https://www.googleapis.com/customsearch/v1?key=AIzaSyBf8ZMDOePSdOUWLQ0bxi0ll3Vs37UcDNM&cx=012894019053467490541:qb36mkgv7wu&q=cats
+
+// Image parameter: searchType=image
+
+
+/* complete image URI (cat search): 
+https://www.googleapis.com/customsearch/v1?key=AIzaSyBf8ZMDOePSdOUWLQ0bxi0ll3Vs37UcDNM&cx=012894019053467490541:qb36mkgv7wu&searchType=image&q=cats
+*/
+
