1fb7ff307b30311dce57aa6ba0d2ecf6426a203d
Jay Ullman <jayullman@gmail.com>
2017-04-11 22:06:32 -0700

    Add touch detection for css styles

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// this will add the class .can-touch to the root element allowing
+// to target touch screen devices
+// from http://www.javascriptkit.com/dhtmltutors/sticky-hover-issue-solutions.shtml
+window.onload = () => {
+  document.addEventListener('touchstart', function addtouchclass(e) {
+    document.documentElement.classList.add('can-touch');
+    document.removeEventListener('touchstart', addtouchclass, false);
+  }, false)
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
